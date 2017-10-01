ab65e2142521821a2766e0b9958d22b40adab3af
Francisco Matheus <francisco.matheus.souza@gmail.com>
2017-10-01 17:04:48 -0300

    Main definitions add

---
+++ definitions.h
@@ -0,0 +1,11 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <vector>
+
+typedef unsigned int uint;
+typedef uint Symbol;
+typedef std::vector<Symbol> Context;
+enum {ESC = 256};
+
+#endif
