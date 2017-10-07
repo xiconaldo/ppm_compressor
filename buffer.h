079c420e31ee8a08aec8c78b3d9d38fb35470f9f
Francisco Matheus <francisco.matheus.souza@gmail.com>
2017-10-07 17:07:18 -0300

    Minor changes

--- buffer.h
+++ buffer.h
@@ -7,8 +7,8 @@ template< class T >
 class Buffer{
 
 public:
-    virtual void operator>>(Symbol& symbol) = 0;
-    virtual void operator<<(const Symbol symbol) = 0;
+    virtual void operator>>(T& symbol) = 0;
+    virtual void operator<<(const T symbol) = 0;
     virtual bool eof() = 0;
     virtual void reset() = 0;
     virtual void print() = 0;
